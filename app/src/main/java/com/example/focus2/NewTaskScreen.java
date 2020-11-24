452d724f4e66a46e9a467188d55ce17cf649c2c6
volpera <gui.volpee@gmail.com>
2020-11-24 05:41:45 -0800

    New Task Screen - Some Fields added

--- app/src/main/java/com/example/focus2/NewTaskScreen.java
+++ app/src/main/java/com/example/focus2/NewTaskScreen.java
@@ -2,32 +2,144 @@ package com.example.focus2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlarmManager;
+import android.app.DatePickerDialog;
+import android.app.PendingIntent;
+import android.app.TimePickerDialog;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.TimePicker;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
 public class NewTaskScreen extends AppCompatActivity {
+
+    final Calendar myCalendar = Calendar.getInstance();
+
+    EditText dueDate;
+    EditText alarm;
+
+    TimePicker myTimePicker;
+    TimePickerDialog timePickerDialog;
+    DatePickerDialog.OnDateSetListener onDateSetListener;
+
+    final static int ONE = 1;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_task_screen);
 
+        //Back to main screen button
         ImageButton backButton = findViewById(R.id.backButton);
-
         backButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 backMainScreen(v);
             }
         });
+
+        //Configuring the dueDate field
+        dueDate = (EditText) findViewById(R.id.dueDate);
+        dueDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openDatePickerDialog();
+                new DatePickerDialog(NewTaskScreen.this, onDateSetListener, myCalendar.get(Calendar.YEAR),
+                        myCalendar.get(Calendar.MONTH),myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
+
+        alarm = findViewById(R.id.alarmTime);
+        alarm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openTimePickerDialog(false);
+            }
+        });
     }
 
+    //To go back to main screen
     public void backMainScreen(View view)
     {
         Intent backMain = new Intent(this, MainActivity.class);
         startActivity(backMain);
     }
+
+    private void openDatePickerDialog() {
+        onDateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                myCalendar.set(Calendar.YEAR, year);
+                myCalendar.set(Calendar.MONTH, month);
+                myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                updateLabel();
+            }
+        };
+    }
+
+    //To show selected due date
+    private void updateLabel() {
+        String myFormat = "MM/dd/yyyy";
+        SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.CANADA);
+
+        dueDate.setText(sdf.format(myCalendar.getTime()));
+    }
+
+    private void openTimePickerDialog(boolean is24r) {
+        Calendar calendar = Calendar.getInstance();
+
+        timePickerDialog = new TimePickerDialog(NewTaskScreen.this,
+                onTimeSetListener, calendar.get(Calendar.HOUR_OF_DAY),
+                calendar.get(Calendar.MINUTE), is24r);
+        timePickerDialog.setTitle("Set Alarm Time");
+
+        timePickerDialog.show();
+    }
+
+    TimePickerDialog.OnTimeSetListener onTimeSetListener = new TimePickerDialog.OnTimeSetListener() {
+
+        @Override
+        public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+
+            Calendar calSet = (Calendar) myCalendar.clone();
+
+            calSet.set(Calendar.HOUR_OF_DAY, hourOfDay);
+            calSet.set(Calendar.MINUTE, minute);
+            calSet.set(Calendar.SECOND, 0);
+            calSet.set(Calendar.MILLISECOND, 0);
+
+
+            //To be fixed                                                               <<<<<<<<
+            /*if (calSet.compareTo(myCalendar) <= 0) {
+                // Today Set time passed, count to tomorrow
+                calSet.add(Calendar.DATE, 1);
+            }
+            */
+            setAlarm(calSet);
+        }
+    };
+
+    private void setAlarm(Calendar targetCal) {
+
+        String myFormat = "h:mm a";
+        SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.CANADA);
+
+        alarm.setText(sdf.format(targetCal.getTime()));
+
+        Intent intent = new Intent(getBaseContext(), AlarmReceiver.class);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(getBaseContext(), ONE, intent, 0);
+        AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+        alarmManager.set(AlarmManager.RTC_WAKEUP, targetCal.getTimeInMillis(), pendingIntent);
+    }
+
 }
